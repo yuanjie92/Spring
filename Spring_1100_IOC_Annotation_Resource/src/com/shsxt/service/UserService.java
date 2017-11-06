e5fe68d5e3c33b0668cc47663c3835bade1fc0b5
yuanjie92 <425697230@qq.com>
2017-11-06 13:20:45 +0800

    @Resource

---
+++ Spring_1100_IOC_Annotation_Resource/src/com/shsxt/service/UserService.java
@@ -0,0 +1,36 @@
+package com.shsxt.service;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.shsxt.dao.UserDao;
+import com.shsxt.model.User;
+
+public class UserService {
+
+	@Resource
+	private UserDao userDao;
+	
+	public UserService(){}
+	
+	
+	public void save(User user){
+		userDao.save(user);
+	}
+	
+	public UserDao getUserDao() {
+		return userDao;
+	}
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
+	
+	public void init(){
+		System.out.println("init");
+	}
+	
+	public void destroy(){
+		System.out.println("destroy");
+	}
+}
