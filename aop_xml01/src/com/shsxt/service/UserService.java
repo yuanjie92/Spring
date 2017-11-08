b1b61ed545b6dcb602045b4595f32505a7221997
yuanjie92 <425697230@qq.com>
2017-11-08 21:45:33 +0800

    aop_xml01

---
+++ aop_xml01/src/com/shsxt/service/UserService.java
@@ -0,0 +1,11 @@
+package com.shsxt.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+	public void save() {
+		System.out.println("save success...");
+	}
+}
